c15f5c22ce556f7439ef632518eb1ea9504c8304
LaserAce <shaunlandy11@hotmail.com>
2016-06-24 20:59:34 +0100

    WorldLayers

---
+++ Assets/Scripts/Test.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Test : MonoBehaviour
+{
+    bool greyWorld = true;
+    void Awake()
+    {
+        Physics2D.IgnoreLayerCollision(8, 10, true);
+    }
+	
+	void Update ()
+    {
+        float _v = Input.GetAxis("Vertical") * Time.deltaTime * 5.0f;
+        float _h = Input.GetAxis("Horizontal") * Time.deltaTime * 5.0f;
+        transform.position += new Vector3(_h, _v, 0.0f);
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            greyWorld = !greyWorld;
+            Physics2D.IgnoreLayerCollision(8, 10, greyWorld);
+            Physics2D.IgnoreLayerCollision(9, 10, !greyWorld);
+        }
+    }
+}
