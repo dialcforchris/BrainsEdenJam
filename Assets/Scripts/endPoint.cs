fe648188edf73299c2eedf04151afe7df9f7f94d
Craig Tinney <ctinney94@gmail.com>
2016-06-25 10:53:31 +0100

    FSAHJKDFHSAJK
    FSDJAKLDFJSAKLDJKLSA

---
+++ Assets/Scripts/endPoint.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class endPoint : MonoBehaviour {
+    
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.tag == "Player")
+        {
+            attractPlayer = true;
+            thing = col.gameObject;
+        }
+    }
+
+    bool attractPlayer;
+    GameObject thing;
+	// Update is called once per frame
+	void Update ()
+    {
+        if (attractPlayer)
+        {
+            thing.transform.position = Vector3.Lerp(thing.transform.position, transform.position, Time.deltaTime*2.5f);
+        }
+    }
+}
