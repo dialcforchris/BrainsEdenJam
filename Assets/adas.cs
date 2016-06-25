c2ecf3e9b9a9da0d043c238cda6c74c394112234
LaserAce <shaunlandy11@hotmail.com>
2016-06-25 22:57:40 +0100

    BetterFPS

---
+++ Assets/adas.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class adas : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	    for(int i = 0; i < transform.childCount; ++i)
+        {
+            for (int j = 0; j < transform.GetChild(i).transform.childCount; ++j)
+            {
+                if(transform.GetChild(i).transform.GetChild(j).transform.GetChild(0).GetComponent<Rigidbody2D>())
+                {
+                    Destroy(transform.GetChild(i).transform.GetChild(j).transform.GetChild(0).GetComponent<Rigidbody2D>());
+                }
+            }
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
