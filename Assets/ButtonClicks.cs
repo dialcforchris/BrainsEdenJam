336a73f4307c94ae549048ba88ef62d279acb4e4
dialcforchris <christopher2.mugridge@live.uwe.ac.uk>
2016-06-26 15:22:45 +0100

    honky

---
+++ Assets/ButtonClicks.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ButtonClicks : MonoBehaviour
+{
+    public AudioClip clip;
+    public AudioClip hahahahahaha;
+    public AudioSource source;
+	public void PlaySound()
+    {
+        AudioClip ac = Random.value < 0.9f ? clip : hahahahahaha;
+        source.PlayOneShot(ac);
+    }
+}
