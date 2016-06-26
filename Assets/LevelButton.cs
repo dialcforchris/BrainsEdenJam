73565238d00cc700cc73d1340be78cf985bc9fd0
Craig Tinney <ctinney94@gmail.com>
2016-06-26 14:12:46 +0100

    Level select done, stars done

---
+++ Assets/LevelButton.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine.UI;
+using UnityEngine;
+using System.Collections;
+
+public class LevelButton : MonoBehaviour {
+
+    public int Index;
+
+    public Text text;
+    public Image[] Stars;
+
+	// Use this for initialization
+	void Start ()
+    {
+        text.text = ""+Index;
+	}
+
+    public void LoadLevel()
+    {
+        Application.LoadLevel(Index);
+    }
+}
---
+++ Assets/LevelSelect.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using System.IO;
+
+public class LevelSelect : MonoBehaviour {
+
+    public LevelButton[] Levels;
+    public Sprite filledStar;
+
+	// Use this for initialization
+	void Start ()
+    {
+        if (!File.Exists(Application.dataPath + "/" + "sters.txt"))
+        {
+            StreamWriter newFile = new StreamWriter(Application.dataPath + "/" + "sters.txt", false);
+            newFile.Close();
+        }
+        StreamReader sr = new StreamReader(Application.dataPath + "/" + "sters.txt");
+        string[] starString = sr.ReadToEnd().Split('\n');
+        
+        foreach (string s in starString)
+        {
+            if (s.Length > 2)
+            {
+                string Levelnum = "" + s[6];
+                string stars = "" + s[s.Length - (s.EndsWith("\n") ? 2 : 1)];
+                for (int i = 0; i < int.Parse(stars); i++)
+                {
+                    Levels[int.Parse(Levelnum) - 1].Stars[i].sprite = filledStar;
+                }
+            }
+        }
+
+        sr.Close();
+    }
+}
