3ee3ff7028e1ec20ae4bcb3d9fa8782c9f16adc0
Craig Tinney <ctinney94@gmail.com>
2016-06-26 09:50:26 +0100

    Stars are ready and working, splash screen slightly modified

---
+++ Assets/Scripts/star.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class star : MonoBehaviour {
+
+    bool collected;
+
+    public Animator anim;
+    public ParticleSystem starPoof;
+
+    void OnTriggerEnter2D()
+    {
+        if (!collected)
+        {
+            Invoke("thing", 0.5f);
+            anim.SetBool("collected", true);
+            collected = true;
+            starManager.instance.starCollection();
+        }
+    }
+    void thing()
+    {
+            starPoof.Play();
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update () {
+	
+	}
+}
---
+++ Assets/Scripts/starManager.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class starManager : MonoBehaviour {
+
+    int starsCollected;
+    public Image[] UIStars;
+    public Sprite FillledStar;
+    public static starManager instance;
+
+	void Start ()
+    {
+        instance = this;
+	}
+
+    public void starCollection()
+    {
+        UIStars[starsCollected].sprite = FillledStar;
+        //Probably do some particle effects too
+        starsCollected++;
+    }
+
+    void Update()
+    {
+
+    }
+}
